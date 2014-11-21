cd82fc1c491a800ce98431ace01057a0997b909d
Alexander Borzunov <hxrussia@gmail.com>
2014-11-21 07:39:50 +0600

    Refactor "server/main.c"

--- server/main.c
+++ server/main.c
@@ -131,18 +131,122 @@ void *start_handle_shortcuts(void *arg) {
     return NULL;
 }
 
-#define MIN_FRAME_DURATION ((NSECS_PER_SEC) / (MAX_FPS))
+
+void send_first_frame(Imlib_Image *image,
+        Window window, int client_width, int client_height, DATA32 **data) {
+    *image = screenshot_get(window,
+            screen_left, screen_top, client_width, client_height);
+    imlib_context_set_image(*image);
+    *data = imlib_image_get_data_for_reading_only();
+    
+    image_send_all(conn_fd, *data, client_width, client_height);
+}
 
 char conn_check_char = CONN_CHECK;
 
+void send_next_frame(Imlib_Image *image,
+        Window window, int client_width, int client_height, DATA32 **data,
+        int region_width, int region_height) {
+    // Check whether connection is dead. If so, SIGPIPE will be sent.
+    if (write(conn_fd, &conn_check_char, sizeof (char)) < 0)
+        throw_exc(ERR_SOCK_SEND);
+    
+    profiler_start(STAGE_SHOT);    
+    Imlib_Image next_image = screenshot_get(window,
+            screen_left, screen_top, client_width, client_height);
+    imlib_context_set_image(next_image);
+    DATA32 *next_data = imlib_image_get_data_for_reading_only();
+    profiler_finish(STAGE_SHOT);
+    
+    unsigned i, j;
+    for (i = 0; i < HEIGHT_DIV; i++)
+        for (j = 0; j < WIDTH_DIV; j++)
+            image_send_diff(
+                conn_fd, *data, next_data,
+                client_width, client_height,
+                j * region_width, i * region_height,
+                region_width, region_height
+            );
+    gib_imlib_free_image_and_decache(*image);
+    *image = next_image;
+    *data = next_data;
+
+    traffic_uncompressed += client_width * client_height * 4;
+    has_stats = 1;
+}
+
+void setup_server() {    
+    serv_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serv_fd < 0)
+        throw_exc(ERR_SOCK_CREATE);
+
+    struct hostent *serv = gethostbyname(server_host);
+    if (serv == NULL)
+        throw_exc(ERR_SOCK_RESOLVE);
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    memcpy(&serv_addr.sin_addr.s_addr, serv->h_addr, serv->h_length);
+    serv_addr.sin_port = htons(server_port);
+    
+    if (bind(serv_fd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0)
+        throw_exc(ERR_SOCK_BIND, server_host, server_port);
+
+    listen(serv_fd, 0);
+}
+
+void accept_client() {
+    struct sockaddr_in *client_addr;
+    socklen_t client_len = sizeof (struct sockaddr_in);
+    conn_fd = accept(serv_fd, (struct sockaddr *) &client_addr, &client_len);
+    if (conn_fd < 0)
+        throw_exc(ERR_SOCK_ACCEPT);
+
+    int header_len = strlen(HEADER);
+    if (read(conn_fd, buffer, header_len + 1) != header_len + 1 ||
+            buffer[header_len] != '\n' || strncmp(buffer, HEADER, header_len))
+        throw_exc(ERR_CLIENT_VERSION);
+}
+
+void perform_handshake(unsigned *client_width, unsigned *client_height) {
+    if (read(conn_fd, buffer, COORD_SIZE * 2) != COORD_SIZE * 2)
+        throw_exc(ERR_SOCK_RECV);
+    int i = -1;
+    READ_COORD(*client_width, buffer, i);
+    READ_COORD(*client_height, buffer, i);
+}
+
+#define MIN_FRAME_DURATION ((NSECS_PER_SEC) / (MAX_FPS))
+
+void perform_mainloop(Window window, int client_width, int client_height) {
+    Imlib_Image image;
+    DATA32 *data;
+    send_first_frame(&image, window, client_width, client_height, &data);
+    
+    unsigned region_width = client_width / WIDTH_DIV;
+    unsigned region_height = client_height / HEIGHT_DIV;
+    while (1) {
+        long long frame_start_time = get_time_nsec();
+        
+        send_next_frame(&image, window, client_width, client_height, &data,
+                region_width, region_height);
+
+        long long frame_duration = get_time_nsec() - frame_start_time;
+        long long sleep_time = MIN_FRAME_DURATION - frame_duration;
+        if (sleep_time > 0)
+            usleep(sleep_time / NSECS_PER_MSEC);
+        else
+        if (stats_enabled && sleep_time < 0)
+            printf("[~] Too slow! Frame duration is %lld ms.\n",
+                    frame_duration / NSECS_PER_MSEC);
+    }
+}
+
 int main(int argc, char *argv[]) {
     set_except(show_error);
-    
     signal(SIGINT, sigint_handler);
     signal(SIGPIPE, sigpipe_handler);
     
     printf("InkMonitor v0.01 Alpha 4 - Server\n");
-    
     parse_arguments(argc, argv);
     
     Screen *screen;
@@ -162,97 +266,15 @@ int main(int argc, char *argv[]) {
     if (pthread_create(&shortcuts_thread, NULL,
             start_handle_shortcuts, &shortcuts_res))
         throw_exc(ERR_THREAD_CREATE);
-
-    serv_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serv_fd < 0)
-        throw_exc(ERR_SOCK_CREATE);
     
-    struct hostent *serv = gethostbyname(server_host);
-    if (serv == NULL)
-        throw_exc(ERR_SOCK_RESOLVE);
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    memcpy(&serv_addr.sin_addr.s_addr, serv->h_addr, serv->h_length);
-    serv_addr.sin_port = htons(server_port);
-    
-    if (bind(serv_fd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0)
-        throw_exc(ERR_SOCK_BIND, server_host, server_port);
-
-    listen(serv_fd, 0);
-    
-    struct sockaddr_in *client_addr;
-    socklen_t client_len = sizeof (client_addr);
+    setup_server();
     printf("[+] Listen on %s:%d\n", server_host, server_port);
-    
-    conn_fd = accept(serv_fd, (struct sockaddr *) &client_addr, &client_len);
-    if (conn_fd < 0)
-        throw_exc(ERR_SOCK_ACCEPT);
-
-    int header_len = strlen(HEADER);
-    if (read(conn_fd, buffer, header_len + 1) != header_len + 1 ||
-            buffer[header_len] != '\n' || strncmp(buffer, HEADER, header_len))
-        throw_exc(ERR_CLIENT_VERSION);
+    accept_client();
     printf("[+] Accepted client connection\n");
-    
-    if (read(conn_fd, buffer, COORD_SIZE * 2) != COORD_SIZE * 2)
-        throw_exc(ERR_SOCK_RECV);
     unsigned client_width, client_height;
-    int i = -1;
-    READ_COORD(client_width, buffer, i);
-    READ_COORD(client_height, buffer, i);
+    perform_handshake(&client_width, &client_height);
     printf("    Reader resolution: %ux%u\n",
             client_width, client_height);
-            
-    unsigned region_width = client_width / WIDTH_DIV;
-    unsigned region_height = client_height / HEIGHT_DIV;
-    
-    Imlib_Image image = screenshot_get(window,
-            screen_left, screen_top, client_width, client_height);
-    imlib_context_set_image(image);
-    DATA32 *data = imlib_image_get_data_for_reading_only();
-    
-    image_send_all(conn_fd, data, client_width, client_height);
-    
-    while (1) {
-        long long frame_start_time = get_time_nsec();
-        
-        // Check whether connection is dead. If so, SIGPIPE will be sent.
-        if (write(conn_fd, &conn_check_char, sizeof (char)) < 0)
-            throw_exc(ERR_SOCK_SEND);
-        
-        profiler_start(STAGE_SHOT);
-        
-        Imlib_Image next_image = screenshot_get(window,
-                screen_left, screen_top, client_width, client_height);
-        imlib_context_set_image(next_image);
-        DATA32 *next_data = imlib_image_get_data_for_reading_only();
-        
-        profiler_finish(STAGE_SHOT);
-        
-        unsigned i, j;
-        for (i = 0; i < HEIGHT_DIV; i++)
-            for (j = 0; j < WIDTH_DIV; j++)
-                image_send_diff(
-                    conn_fd, data, next_data,
-                    client_width, client_height,
-                    j * region_width, i * region_height,
-                    region_width, region_height
-                );
-                
-        gib_imlib_free_image_and_decache(image);
-        image = next_image;
-        data = next_data;
-    
-        traffic_uncompressed += client_width * client_height * 4;
-        has_stats = 1;
-        
-        long long frame_duration = get_time_nsec() - frame_start_time;
-        long long sleep_time = MIN_FRAME_DURATION - frame_duration;
-        if (sleep_time > 0)
-            usleep(sleep_time / NSECS_PER_MSEC);
-        else
-        if (stats_enabled && sleep_time < 0)
-            printf("[~] Too slow! Frame duration is %lld ms.\n",
-                    frame_duration / NSECS_PER_MSEC);
-    }
+    perform_mainloop(window, client_width, client_height);
+    return 0;
 }
